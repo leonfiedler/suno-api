27987ca4d14c61d40285747021298eba3aaed871
Leon  Fiedler <leon-fiedler@hotmail.de>
2024-04-02 20:50:07 -0700

    removed Allow-Origin restriction

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("IP_ORIGIN")],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
